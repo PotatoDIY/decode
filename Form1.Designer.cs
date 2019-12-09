dd5be75d4c0d3a1c18ecc0b000f7195f15c3d6e7
土豆电工 <tudoudiangong@163.com>
2019-12-10 07:24:56 +0800

    ..

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Decode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDe = new System.Windows.Forms.Button();
+            this.textBoxString = new System.Windows.Forms.TextBox();
+            this.textBoxEnString = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonDe
+            // 
+            this.buttonDe.Location = new System.Drawing.Point(12, 231);
+            this.buttonDe.Name = "buttonDe";
+            this.buttonDe.Size = new System.Drawing.Size(776, 50);
+            this.buttonDe.TabIndex = 5;
+            this.buttonDe.Text = "DeCode";
+            this.buttonDe.UseVisualStyleBackColor = true;
+            this.buttonDe.Click += new System.EventHandler(this.buttonDe_Click);
+            // 
+            // textBoxString
+            // 
+            this.textBoxString.Location = new System.Drawing.Point(12, 299);
+            this.textBoxString.Multiline = true;
+            this.textBoxString.Name = "textBoxString";
+            this.textBoxString.Size = new System.Drawing.Size(776, 199);
+            this.textBoxString.TabIndex = 4;
+            // 
+            // textBoxEnString
+            // 
+            this.textBoxEnString.Location = new System.Drawing.Point(12, 12);
+            this.textBoxEnString.Multiline = true;
+            this.textBoxEnString.Name = "textBoxEnString";
+            this.textBoxEnString.Size = new System.Drawing.Size(776, 199);
+            this.textBoxEnString.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 513);
+            this.Controls.Add(this.buttonDe);
+            this.Controls.Add(this.textBoxString);
+            this.Controls.Add(this.textBoxEnString);
+            this.Name = "Form1";
+            this.Text = "DeCode for RSA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDe;
+        private System.Windows.Forms.TextBox textBoxString;
+        private System.Windows.Forms.TextBox textBoxEnString;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Decode
+{
+    public partial class Form1 : Form
+    {
+        RSACryptoServiceProvider rsa;
+        public Form1()
+        {
+            InitializeComponent();
+            if (File.Exists("PrivateKey.xml"))
+            {
+                using (StreamReader streamReader = new StreamReader("PrivateKey.xml")) // 读取运行目录下的PublicKey.xml
+                {
+                    rsa = new RSACryptoServiceProvider();
+                    rsa.FromXmlString(streamReader.ReadToEnd()); // 将公匙载入进RSA实例中
+                }
+            }
+            else
+            {
+                MessageBox.Show("软件根目录缺少公钥文件：PrivateKey.xml");
+                Application.Exit();
+            }
+        }
+
+        string RSADecrypt(string content)
+        {
+            string decryptedContent = string.Empty;
+            byte[] decryptedData = rsa.Decrypt(Convert.FromBase64String(content), false);
+            decryptedContent = Encoding.GetEncoding("utf-8").GetString(decryptedData);
+            return decryptedContent;
+        }
+        private void buttonDe_Click(object sender, EventArgs e)
+        {
+            textBoxString.Text = RSADecrypt(textBoxEnString.Text);
+        }
+    }
+}
